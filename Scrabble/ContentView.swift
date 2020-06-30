7ce847d5b9928d8f910b7ed3fa9729876fab2a14
zoie <zoietady@gmail.com>
2020-06-30 10:10:14 +1000

    Initial Commit

---
+++ Scrabble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrabble
+//
+//  Created by Zoie Tad-y on 6/30/20.
+//  Copyright © 2020 Zoie Tad-y. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
